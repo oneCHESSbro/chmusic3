f9d57515e679afce28384de5c40a574326062da7
oneCHESSbro <75912049+oneCHESSbro@users.noreply.github.com>
2021-02-20 04:57:42 +0400

    Add files via upload

---
+++ events/ready.js
@@ -0,0 +1,26 @@
+const chalk = require('chalk');
+const moment = require('moment');
+const Discord = require('discord.js');
+const ayarlar = require('../ayarlar.json');
+
+var prefix = ayarlar.prefix;
+
+module.exports = client => {
+var oyun = [
+        "RTX Müzik",
+        "xxyardım"
+    ];
+
+    setInterval(function() {
+
+        var random = Math.floor(Math.random()*(oyun.length-0+1)+0);
+
+        client.user.setActivity(oyun[random], "RTX Müzik | xxyardım" );
+        }, 2 * 2500);
+    
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Aktif, Komutlar yüklendi!`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: ${client.user.username} ismi ile giriş yapıldı!`);
+  client.user.setStatus("dnd");
+  client.user.setActivity(`Mechanic Müzik | p!komutlar`);
+  console.log(`[${moment().format('YYYY-MM-DD HH:mm:ss')}] BOT: Oyun ismi ayarlandı!`);
+};
